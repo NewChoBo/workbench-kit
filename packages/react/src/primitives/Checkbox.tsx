1f3caa945f4b5e5dc2c8e5f1889f2d9db1167a27
NewChoBo <whwjdrb1009@gmail.com>
2026-06-05 01:12:38 +0900

    feat(react): support reusable property controls

--- packages/react/src/primitives/Checkbox.tsx
+++ packages/react/src/primitives/Checkbox.tsx
@@ -2,14 +2,14 @@ import type { ComponentPropsWithRef, ReactNode } from 'react';
 import { cx } from '../utils/cx';
 
 export interface CheckboxProps extends Omit<ComponentPropsWithRef<'input'>, 'type'> {
-  label: ReactNode;
+  label?: ReactNode | undefined;
 }
 
 export function Checkbox({ className, label, ...props }: CheckboxProps) {
   return (
     <label className={cx('ui-checkbox', className)}>
       <input type="checkbox" {...props} />
-      <span>{label}</span>
+      {label !== undefined && label !== null && label !== '' ? <span>{label}</span> : null}
     </label>
   );
 }
